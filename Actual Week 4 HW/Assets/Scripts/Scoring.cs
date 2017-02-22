a82cc93427559ae2bfcfd81a9e16f2b81dd9f7ba
Unknown <andrewpetersell@gmail.com>
2017-02-21 22:25:00 -0500

    High Score Set Working

--- Actual Week 4 HW/Assets/Scripts/Scoring.cs
+++ Actual Week 4 HW/Assets/Scripts/Scoring.cs
@@ -4,8 +4,8 @@ using UnityEngine;
 using UnityEngine.UI;
 
 public class Scoring : MonoBehaviour {
+
 	public float score; 
-	public float finalScore;
 	Text numerals; 
 	GameObject bossHealth;
 	BossHealth bh;
@@ -17,19 +17,40 @@ public class Scoring : MonoBehaviour {
 
 	private const string PREF_BEST_TIME = "bestTimePref";
 
-	private int bestTime = 30;
 
-	public int BestTime 
+
+	private float finalScore; 
+
+	public float FinalScore 
+	{
+		get
+		{
+			return finalScore;	
+		}
+
+		set
+		{
+			finalScore = value; 
+			if (finalScore <= BestTime) 
+			{
+				BestTime = FinalScore; 
+			}
+		}
+	}
+
+	private float bestTime;
+
+	public float BestTime
 	{
 		get 
 		{
-			bestTime = PlayerPrefs.GetInt(PREF_BEST_TIME);
+			bestTime = PlayerPrefs.GetFloat(PREF_BEST_TIME);
 			return bestTime; 
 		}
 		set 
 		{ 
 			bestTime = value;
-			PlayerPrefs.SetInt(PREF_BEST_TIME, bestTime);
+			PlayerPrefs.SetFloat(PREF_BEST_TIME, bestTime);
 		}
 	}
 
@@ -41,11 +62,17 @@ public class Scoring : MonoBehaviour {
 		bh = bossHealth.GetComponent<BossHealth> ();
 		finalWord = endText.GetComponent<Text> (); 
 		finalScoreDisplay = endScore.GetComponent<Text> ();
+		if (BestTime == 0) 
+		{
+			BestTime = 99;
+		}
 		
 	} 
 	
 	// Update is called once per frame
 	void Update () {
+
+		Debug.Log ("Current Best Time: " + bestTime);
 
 
 
@@ -60,9 +87,9 @@ public class Scoring : MonoBehaviour {
 
 		if (bh.itsOver) 
 		{
-			finalScore = score; 
+			FinalScore = score; 
 			finalScoreDisplay.text = finalScore.ToString ();
-			if (finalScore <= BestTime) {
+			if (FinalScore <= BestTime) {
 				finalWord.text = "You Win! NEW BEST TIME: ";
 			} else {
 					finalWord.text = "You Win! Your Time: ";
